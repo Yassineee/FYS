4d232f281213862ae8d2b9b9228b018bbf8d03eb
Yassineee <yassine_elabdellaoui@hotmail.nl>
2014-11-20 15:22:35 +0100

    alle labels aangemaakt!

--- src/Manager/Manager.java
+++ src/Manager/Manager.java
@@ -2,6 +2,8 @@ package Manager;
 
 
 import Manager.Managerhome;
+import Manager.Managerhome;
+import Manager.Managerhome;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,12 +14,12 @@ import Manager.Managerhome;
  *
  * @author Yassinee
  */
-public class ManagerStatus extends javax.swing.JFrame {
+public class Manager extends javax.swing.JFrame {
 
     /**
      * Creates new form Managerhome
      */
-    public ManagerStatus() {
+    public Manager() {
         initComponents();
     }
 
@@ -38,6 +40,13 @@ public class ManagerStatus extends javax.swing.JFrame {
         AddBaggage2 = new javax.swing.JLabel();
         AddBaggage3 = new javax.swing.JLabel();
         ID = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        ID1 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jCheckBox2 = new javax.swing.JCheckBox();
+        jCheckBox3 = new javax.swing.JCheckBox();
+        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -46,13 +55,13 @@ public class ManagerStatus extends javax.swing.JFrame {
 
         Name.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         Name.setText("Logged in as:");
-        getContentPane().add(Name, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, 140, 50));
+        getContentPane().add(Name, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 140, 50));
 
         Home.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         Home.setText("Home");
         getContentPane().add(Home, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 190, 70, 30));
 
-        Search.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        Search.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         Search.setText("Search");
         getContentPane().add(Search, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 250, 90, 40));
 
@@ -64,7 +73,7 @@ public class ManagerStatus extends javax.swing.JFrame {
         AddBaggage1.setText("Graphs");
         getContentPane().add(AddBaggage1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, -1, -1));
 
-        AddBaggage2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        AddBaggage2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         AddBaggage2.setText("Status");
         getContentPane().add(AddBaggage2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 410, -1, -1));
 
@@ -73,14 +82,71 @@ public class ManagerStatus extends javax.swing.JFrame {
         getContentPane().add(AddBaggage3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 480, -1, -1));
 
         ID.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        ID.setText("Employee ID:");
-        getContentPane().add(ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 90, 130, 50));
+        ID.setText("Search");
+        getContentPane().add(ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 140, 130, 50));
+
+        jTextField1.setText("Search");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 180, 690, 30));
+
+        ID1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        ID1.setText("Employee ID:");
+        getContentPane().add(ID1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 70, 130, 50));
+
+        jCheckBox1.setText("Name");
+        jCheckBox1.setToolTipText("");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 210, -1, -1));
+
+        jCheckBox2.setText("Color");
+        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jCheckBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 210, -1, -1));
+
+        jCheckBox3.setText("Status");
+        getContentPane().add(jCheckBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 210, -1, -1));
+
+        jButton1.setText("Search");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 180, -1, 30));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 270, 710, 280));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/layoutCorendonFinalVeranderd.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -40, 1300, 740));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, -50, 1300, 740));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,8 +190,15 @@ public class ManagerStatus extends javax.swing.JFrame {
     private javax.swing.JLabel AddBaggage3;
     private javax.swing.JLabel Home;
     private javax.swing.JLabel ID;
+    private javax.swing.JLabel ID1;
     private javax.swing.JLabel Name;
     private javax.swing.JLabel Search;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JCheckBox jCheckBox3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
